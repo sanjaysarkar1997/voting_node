329e487ddc7cd058ce6b8e3f1623cda29a9fc068
Saup21 <sauparna.gupta@gmail.com>
2023-04-25 22:44:04 +0530

    added otp

--- backend/data/data.js
+++ backend/data/data.js
@@ -2,42 +2,52 @@ const walletData = [
   {
     phoneNo: "1234567890",
     name: "John Doe (Chairperson)",
+    otp: 1111,
   },
   {
     phoneNo: "1234567891",
     name: "Jane Doe",
+    otp: 2222,
   },
   {
     phoneNo: "1234567892",
     name: "John Smith",
+    otp: 3333,
   },
   {
     phoneNo: "1234567893",
     name: "Jane Smith",
+    otp: 4444,
   },
   {
     phoneNo: "1234567894",
     name: "Natish Rana",
+    otp: 5555,
   },
   {
     phoneNo: "1234567895",
     name: "Jui Rana",
+    otp: 6666,
   },
   {
     phoneNo: "1234567896",
     name: "Rajesh Rana",
+    otp: 7777,
   },
   {
     phoneNo: "1234567897",
     name: "Rajeshwari Rana",
+    otp: 8888,
   },
   {
     phoneNo: "1234567898",
     name: "John Wick",
+    otp: 9999,
   },
   {
     phoneNo: "1234567899",
     name: "Robin Wick",
+    otp: 0000,
   },
 ];
 
