a6b2926b5079de8e80afff2b0c0581fd60907733
Sanjay Sarkar <sanjaysarkar430@gmail.com>
2023-04-28 23:05:29 +0530

    update

--- frontend/src/apis/http.ts
+++ frontend/src/apis/http.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // create an axios instance
 const service = axios.create({
-  baseURL: "http://localhost:8000/api/", // url = base url + request url
+  baseURL: "https://elegant-jade-gopher.cyclic.app/api/", // url = base url + request url
 });
 
 export default service;
